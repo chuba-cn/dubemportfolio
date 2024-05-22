2275601b17e95a729c674b790f37f16e3ef7296f
Chuba-Nwene. C <chinemelumchubanwene57@gmail.com>
2024-05-22 15:59:59 +0200

    remove 3d-pin component and add card with tracing beam on scroll from project section.

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,51 +1,80 @@
 "use client";
-import React from "react";
-import { PinContainer } from "./ui/3d-pin";
+
 import { projects } from "@/constants";
-import { MoveUpRight } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
+import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
+import { TracingBeam } from "./ui/tracing-beam";
 
 export default function ProjectList() {
   return (
-    <div className="flex flex-wrap items-center justify-center p-4 gap-12">
-      {projects.map((item) => (
-        <div
-          className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-          key={item.title}
-        >
-          <PinContainer title="Visit" href={item.url}>
-            <div className="relative flex flex-col items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[35vh] lg:h-[45vh] mb-5">
-              
-            <h1 className="font-bold lg:text-2xl md:text-xl text-base mb-2">
-              {item.title}
-            </h1>
-            <p
-              className="lg:text-lg lg:font-normal font-light text-sm line-clamp-6 text-gray-700"
-              style={{
-                margin: "1vh 0",
-              }}
-            >
-              {item.description}
-            </p>
-            </div>
+    <>
+      <TracingBeam className="px-6 max-sm:hidden">
+        <div className="max-w-2xl mx-auto antialiased pt-4 relative">
+          {projects.map((item) => (
+            <div key={item.title} className="mb-10">
+              <Card>
+                <CardHeader>
+                  <CardTitle className="text-gray-700 font-bold">
+                    {item.title}
+                  </CardTitle>
+                </CardHeader>
 
-            <div className="flex items-center justify-between mt-1 mb-2">
-              <div className="flex justify-center items-center gap-1">
-                <Link
-                  href={item.url}
-                  target="_blank"
-                  className="flex items-center justify-center gap-1 text-primary hover:text-blue-400"
-                >
-                  <p className="lg:text-xl md:text-xs text-sm">
-                    View project details
-                  </p>
-                  <MoveUpRight className="h-4 w-4"/>
-                </Link>
-              </div>
+                <CardContent className="text-gray-700">
+                  {item.description}
+                </CardContent>
+                <CardContent>
+                  <div className="flex justify-start items-center gap-1">
+                    <Link
+                      href={item.url}
+                      target="_blank"
+                      className="flex items-center justify-center gap-1 text-blue-500 hover:text-primary"
+                    >
+                      <p className="lg:text-xl text-lg">View project details</p>
+                      <ArrowUpRight className="h-4 w-4" />
+                    </Link>
+                  </div>
+                </CardContent>
+              </Card>
             </div>
-          </PinContainer>
+          ))}
         </div>
-      ))}
-    </div>
+      </TracingBeam>
+
+      <div className="max-w-2xl mx-auto antialiased pt-4 relative lg:hidden">
+          {projects.map((item) => (
+            <div key={item.title} className="mb-10">
+              <Card>
+                <CardHeader>
+                  <CardTitle className="text-gray-700 font-bold">
+                    {item.title}
+                  </CardTitle>
+                </CardHeader>
+
+                <CardContent className="text-gray-700">
+                  {item.description}
+                </CardContent>
+                <CardContent>
+                  <div className="flex justify-start items-center gap-1">
+                    <Link
+                      href={item.url}
+                      target="_blank"
+                      className="flex items-center justify-center gap-1 text-blue-500 hover:text-primary"
+                    >
+                      <p className="lg:text-xl text-lg">View project details</p>
+                      <ArrowUpRight className="h-4 w-4" />
+                    </Link>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          ))}
+        </div>
+    </>
   );
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,8 +5,8 @@ import ProjectList from "./ProjectList";
 
 const Projects = () => {
   return (
-    <section className="w-full mt-20 md:mt-32" id="projects">
-      <h1 className="text-4xl md:text-6xl font-bold tracking-tight text-wrap text-gray-700 ">
+    <section className="w-full mt-20 md:mt-32 md:mb-32" id="projects">
+      <h1 className="text-4xl md:text-6xl font-bold tracking-tight text-wrap text-gray-700 mb-10 md:mb-16 ">
         Projects{" "}
         <SwordsIcon className="inline text-primary font-bold h-8 w-8" />
       </h1>
